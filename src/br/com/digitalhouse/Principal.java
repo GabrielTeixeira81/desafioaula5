6d961f22e6573144ccb3d84b20fae7e7e6b3fd83
GabrielTeixeira81 <gatecosvh>
2019-04-05 22:13:43 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Principal.java
@@ -0,0 +1,39 @@
+package br.com.digitalhouse;
+
+public class Principal {
+
+    public static String maeDe(Filho filho){
+        return filho.getMae();
+            }
+
+    public static String paiDe(Filho filho){
+        return filho.getPai();
+    }
+    public static boolean mesmaMae(Filho filhoUm, Filho filhoDois){
+        return maeDe(filhoUm) == maeDe(filhoDois);
+    }
+    public static boolean mesmoPai(Filho filhoUm, Filho filhoDois){
+        if (paiDe(filhoUm) == paiDe(filhoDois)){
+            return true;
+        }else {
+            return false;
+        }
+
+}
+public static boolean saoMeioIrmaos(Filho filhoUm, Filho filhoDois){
+    return mesmaMae(filhoUm, filhoDois) && !mesmoPai(filhoUm,filhoDois) || !mesmaMae(filhoUm,filhoDois) && mesmoPai(filhoUm,filhoDois);
+    }
+
+    //--------------
+    public static void main(String[] args) {
+
+        Filho filhoUm = new Filho("Maria", "jorge", "Jose");
+        Filho filhoDois = new Filho("Maria", "Fernando", "Ana");
+
+        mesmaMae(filhoUm, filhoDois);
+        mesmoPai(filhoUm,filhoDois);
+
+        System.out.println(saoMeioIrmaos(filhoUm,filhoDois));
+    }
+
+}
